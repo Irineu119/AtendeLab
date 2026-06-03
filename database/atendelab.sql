6117df46c97a3f4f2e1eb6edb55a7fab41da5dd3
Anthony7az <rayoanthony24@gmail.com>
2026-06-02 21:25:22 -0300

    Modificação da script sql para que a coluna perfil da tabela usuarios aceite o valor aluno

--- database/atendelab.sql
+++ database/atendelab.sql
@@ -6,7 +6,7 @@ CREATE TABLE IF NOT EXISTS usuarios (
     nome VARCHAR(100) NOT NULL,
     email VARCHAR(100) NOT NULL UNIQUE,
     senha VARCHAR(255) NOT NULL,
-    perfil ENUM('admin', 'atendente') DEFAULT 'atendente',
+    perfil ENUM('admin', 'atendente', 'aluno') DEFAULT 'atendente',
     status ENUM('ativo', 'inativo') DEFAULT 'ativo',
     criado_em TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
